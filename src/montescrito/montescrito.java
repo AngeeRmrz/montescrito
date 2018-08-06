527d8457e05c8c1b255a053d915daa19e1683307
Paola Ramirez <paola.ramirez1@utp.edu.co>
2018-08-05 21:49:04 -0500

    inicio.

---
+++ src/montescrito/montescrito.java
@@ -0,0 +1,14 @@
+package montescrito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class montescrito {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
